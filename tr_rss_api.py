c92cfbbedd096b87b45ed296ca97b6b3d9f924d5
Guru Prasath <guru98@live.com>
2018-11-09 20:50:32 +0530

    creating RSS Feed

--- tr_rss_api.py
+++ tr_rss_api.py
@@ -9,7 +9,7 @@ Created on Sun Oct  7 12:40:50 2018
 from multiprocessing import Pool
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import re
+import re,rfeed,datetime
 
 url = "http://tamilrockers.ph"
 html = urlopen(url)
@@ -22,11 +22,12 @@ tnm = tnmheader.find_next("strong")
 nms = str(tnm).split("<br/>")
 
 movie_list = {}
+item_list = []
 
 def fun(nm):  
     nm = BeautifulSoup(nm,'lxml')
     name = nm.text.split(' - [')[0]
-    links = {}
+    data = []
     for post in nm.find_all('a'):
         post_name = post.text
         post_url = post.attrs['href']
@@ -34,15 +35,33 @@ def fun(nm):
             post_html=urlopen(post_url)
             post_soup=BeautifulSoup(post_html,'lxml')
             mag_tag = post_soup.find('a',attrs={'href':re.compile('magnet')})
+            tor_tag = post_soup.find('a',attrs={'title':re.compile('Download attachment')})
             mag_link = mag_tag.attrs['href']
+            tor_link = tor_tag.attrs['href']
+            data.append((post_name,post_url,tor_link,mag_link))
         except:
             mag_link = None
-        links[post_name] = mag_link
+            tor_link = None
         #print("%s:%s => %s"%(name,post_name,mag_link))
-    return(name,links)
+    return(name,data)
 
 with Pool(16) as p:
-    for name,links in p.map(fun,nms):
-        movie_list[name]=links
+    for name,data in p.map(fun,nms):
+        movie_list[name]= data
+        for ptype,purl,torurl,magurl in data:
+            item_list.append(rfeed.Item(
+                                        title=name+' : '+ptype,
+                                        link=purl,
+                                        enclosure=rfeed.Enclosure(magurl,10000,'application/x-bittorrent')
+                                        ))
 
-print(len(movie_list))
+#print(len(movie_list))
+
+feed = rfeed.Feed(
+    title = "TR RSS Feed",
+    description = "Unofficial RSS feed for TR",
+    link = "localhost",
+    lastBuildDate = datetime.datetime.now(),
+    items = item_list)
+
+print(feed.rss())
